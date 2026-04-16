4b099c6cfa325b416801ab590bf070698ef0ff1b
Mustafa Shaheen <68672770+musalem@users.noreply.github.com>
2026-04-16 17:58:10 +0200

    Add files via upload

---
+++ downloadlinks.js
@@ -0,0 +1,10 @@
+const DOWNLOAD_LINKS = {
+    mini: {
+        url: "https://yourdomain.com/mini-v1.2.zip",
+        version: "1.12"
+    },
+    lite: {
+        url: "https://yourdomain.com/lite-v1.1.zip",
+        version: "1.1"
+    }
+};
