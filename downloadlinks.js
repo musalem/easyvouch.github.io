31ff9d05c36a58022c6bdfaf6dc0969272666118
Mustafa Shaheen <68672770+musalem@users.noreply.github.com>
2026-04-16 18:18:39 +0200

    Update download links to new shortened URLs

--- downloadlinks.js
+++ downloadlinks.js
@@ -1,10 +1,10 @@
 const DOWNLOAD_LINKS = {
     mini: {
-        url: "https://yourdomain.com/mini-v1.2.zip",
+        url: "https://bit.ly/4m1UueQ",
         version: "1.12"
     },
     lite: {
-        url: "https://yourdomain.com/lite-v1.1.zip",
+        url: "https://bit.ly/48jeQKO",
         version: "1.1"
     }
 };
